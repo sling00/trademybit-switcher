ab306d164527af1876b89c353fdd726579d819db
Sling00 <sling00@gmail.com>
2014-06-04 00:58:40 -0500

    Whoopsie

--- scripts/scrypt.pl
+++ scripts/scrypt.pl
@@ -10,7 +10,7 @@ my $conffile = "/opt/ifmi/poolmanager.conf";
  my $minerpath = $conf{miners}{$currentm}{mpath};
  my $output = "";
 if ($currentm == $swopt) {
-	print "Best profile is current profile, but miner is not running. Attempting to start."
+	print "Best profile is current profile, but miner is not running. Attempting to start.";
 	&startMining;
 	} else {
 	   print "\nCurrent Profile: " . $conf{miners}{$currentm}{mconfig} . "\n";
