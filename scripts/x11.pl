e24849930968be30e63ab709c45c79cfd20d4ca5
Sling00 <sling00@gmail.com>
2014-06-04 00:51:54 -0500

    Testing additions to PM scripts 2

--- scripts/x11.pl
+++ scripts/x11.pl
@@ -8,8 +8,9 @@ require '/opt/ifmi/pm-common.pl';
 my $conffile = "/opt/ifmi/poolmanager.conf";
  my $currentm = $conf{settings}{current_mconf};
  my $minerpath = $conf{miners}{$currentm}{mpath};
-
-   print "\nCurrent Profile: " . $conf{miners}{$currentm}{mconfig} . "\n\n"; 
+ my $output = "";
+   print "\nCurrent Profile: " . $conf{miners}{$currentm}{mconfig} . "\n";
+   print "Switching to profile: " . $conf{miners}{$swopt}{mconfig} . "\n";
  my $swopt = "5";
 if ($currentm == $swopt) {
 	&startMining;
